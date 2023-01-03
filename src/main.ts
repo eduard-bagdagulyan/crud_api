64ab9ac6cded7279fd9cc2790834302adf4970f7
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2023-01-03 17:26:51 +0400

    added .env file support

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { AppFactory } from './app'
+import * as dotenv from 'dotenv'
+import * as process from 'process'
+
+dotenv.config()
 
 function bootstrap() {
     const app = new AppFactory().create()
+    const APP_PORT = process.env.APP_PORT || 8999
 
-    app.listen(8999) // TODO change to env var
-    console.log('Application starts listening on PORT') // TODO change to env var
+    app.listen(APP_PORT)
+    console.log(
+        `The server starts listening on http://localhost:${APP_PORT}`
+    )
 }
 
 bootstrap()
